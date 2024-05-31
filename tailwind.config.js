cf684a0d11eba6b703568562ee633a5ff81b71fd
Palak <palaksehgal0707@gmail.com>
2024-05-31 15:20:27 +0530

    color change

--- tailwind.config.js
+++ tailwind.config.js
@@ -13,7 +13,7 @@ module.exports = {
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
       colors: {
-        primary: "#115641",
+        primary: "#41bcc3",
         secondary: "#ecd53a",
       },
       screens: {
