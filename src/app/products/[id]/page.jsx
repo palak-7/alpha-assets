9c640d64aca43da6a656edbe193fc283b194a907
Abhishekmathur00 <abhishekmathur7575@gmail.com>
2025-01-14 16:24:53 +0530

    add new content in product page

--- src/app/products/[id]/page.jsx
+++ src/app/products/[id]/page.jsx
@@ -6,17 +6,22 @@ const page = ({ params }) => {
   const p = product.filter((x) => x.id == id);
   return (
     <div className="xl:mt-[200px] mt-[250px] dark:text-black">
-      <h1 className="text-center text-5xl mb-7 font-serif font-bold">
+      <h1 className="text-center text-3xl lg:text-5xl font-serif font-bold mb-4">
         {p[0].name}
       </h1>
-      <div className="grid grid-cols-2">
-        <Image
-          src={`/products/${p[0].image}`}
-          width={1000}
-          height={1000}
-          alt="product"
-          className="w-[500px] h-[300px] mx-10 border-primary border-4 shadow-lg"
-        />
+      <p className="text-center text-xl lg:text-2xl font-serif font-medium mb-6">
+        {p[0].content}
+      </p>
+      <div className="flex flex-col lg:flex-row items-center lg:items-start gap-6">
+        <div className="w-full lg:w-1/2 flex justify-center px-5">
+          <Image
+            src={`/products/${p[0].image}`}
+            width={500}
+            height={300}
+            alt="product"
+            className="w-full max-w-md lg:max-w-full border-primary border-4 shadow-lg rounded-md"
+          />
+        </div>
       </div>
     </div>
   );
--- src/components/product.js
+++ src/components/product.js
@@ -1,51 +1,58 @@
 export const product = [
   {
     id: 1,
-    name: "Premium Floors and Villas",
-    image: "villa.png",
-    content:
-      "India has achieved remarkable success in cancer treatments, thanks to our embrace of new technology and the evolution of our doctors, who are supported by groundbreaking research. The treatment of any cancer depends on various factors, including the type of cancer, the stage of the disease, the cell type of the cancer cells, and whether the disease has spread to other parts of the body (Metastasis). Based on comprehensive investigative outcomes, treatment options range from surgical excision to chemotherapy, radiation therapy, or a combination of these therapies. Today, cancer treatment in India is equipped with cutting-edge diagnostic technologies and advanced treatment modalities. <br/><br/>Contact us for the best oncology treatment available.",
+    // name: "Premium Floors and Villas",
+    name: "Commercial Real Estate (CRE)",
+    image: "cre.png",
+    // content: "India has achieved remarkable success in cancer treatments, thanks to our embrace of new technology and the evolution of our doctors, who are supported by groundbreaking research. The treatment of any cancer depends on various factors, including the type of cancer, the stage of the disease, the cell type of the cancer cells, and whether the disease has spread to other parts of the body (Metastasis). Based on comprehensive investigative outcomes, treatment options range from surgical excision to chemotherapy, radiation therapy, or a combination of these therapies. Today, cancer treatment in India is equipped with cutting-edge diagnostic technologies and advanced treatment modalities. <br/><br/>Contact us for the best oncology treatment available.",
+    content: "Generating stable, recurring income",
+
   },
   {
     id: 2,
-    name: "Condominiums",
-    image: "Condominium.png",
-    content:
-      "India has made significant advancements in orthopedic surgery, driven by our adoption of new technologies and the continuous development of our skilled surgeons, supported by pioneering research. The treatment of orthopedic conditions depends on various factors, including the type of injury or disorder, the severity of the condition, the patient’s overall health, and their response to previous treatments. Based on thorough diagnostic evaluations, treatment options may include minimally invasive surgeries, joint replacements, arthroscopy, and rehabilitation therapies. Today, orthopedic surgery in India is equipped with state-of-the-art diagnostic tools and advanced surgical techniques. <br/><br/>Contact us to receive the best orthopedic care available.",
+    // name: "Condominiums",
+    name: "Green Investment",
+    image: "green-investment.png",
+    // content: "India has made significant advancements in orthopedic surgery, driven by our adoption of new technologies and the continuous development of our skilled surgeons, supported by pioneering research. The treatment of orthopedic conditions depends on various factors, including the type of injury or disorder, the severity of the condition, the patient’s overall health, and their response to previous treatments. Based on thorough diagnostic evaluations, treatment options may include minimally invasive surgeries, joint replacements, arthroscopy, and rehabilitation therapies. Today, orthopedic surgery in India is equipped with state-of-the-art diagnostic tools and advanced surgical techniques. <br/><br/>Contact us to receive the best orthopedic care available.",
+    content: "opportunities in the sustainable sectors"
   },
   {
     id: 3,
-    name: "Retails and Food Courts ",
-    image: "FoodCourts.png",
-    content:
-      "India has made groundbreaking strides in neurosurgery, particularly with the integration of advanced technologies such as the CyberKnife system. Our progress is driven by the expertise of our neurosurgeons, backed by pioneering research and a commitment to adopting innovative treatments. The approach to neurosurgery varies based on the specific neurological condition, its severity, the patient's overall health, and previous treatments. Our comprehensive diagnostic evaluations guide the selection of the most effective treatment options, which may include traditional neurosurgery, minimally invasive techniques, or the use of the CyberKnife system for precise, non-invasive treatment of tumors and other conditions. Today, neurosurgery in India is equipped with cutting-edge diagnostic tools and advanced treatment modalities like the CyberKnife. <br/><br/>Contact us to access the best neurosurgical care and the latest in CyberKnife technology.",
+    // name: "Retails and Food Courts ",
+    name: "Warehousing , Data Centres & Infra Assets",
+    image: "warehouse-ds.png",
+    // content: "India has made groundbreaking strides in neurosurgery, particularly with the integration of advanced technologies such as the CyberKnife system. Our progress is driven by the expertise of our neurosurgeons, backed by pioneering research and a commitment to adopting innovative treatments. The approach to neurosurgery varies based on the specific neurological condition, its severity, the patient's overall health, and previous treatments. Our comprehensive diagnostic evaluations guide the selection of the most effective treatment options, which may include traditional neurosurgery, minimally invasive techniques, or the use of the CyberKnife system for precise, non-invasive treatment of tumors and other conditions. Today, neurosurgery in India is equipped with cutting-edge diagnostic tools and advanced treatment modalities like the CyberKnife. <br/><br/>Contact us to access the best neurosurgical care and the latest in CyberKnife technology.",
+    content: "Critical for long term annuity return."
   },
   {
     id: 4,
-    name: "Office Spaces",
-    image: "officeSpaces.png",
-    content:
-      "India has made remarkable advancements in spine surgery, driven by the integration of cutting-edge technologies and the expertise of our highly skilled surgeons, supported by groundbreaking research. The approach to spine surgery depends on various factors, including the type and severity of the spinal condition, the patient’s overall health, and their response to previous treatments. Based on thorough diagnostic evaluations, treatment options may include minimally invasive spine surgery, spinal fusion, decompression surgery, or advanced procedures like robotic-assisted surgery. Today, spine surgery in India is equipped with state-of-the-art diagnostic tools and advanced surgical techniques. <br/><br/>Contact us to receive the best spine care available.",
+    // name: "Office Spaces",
+    name: "Residential and Land Acquisition",
+    image: "residential.jpg",
+    // content: "India has made remarkable advancements in spine surgery, driven by the integration of cutting-edge technologies and the expertise of our highly skilled surgeons, supported by groundbreaking research. The approach to spine surgery depends on various factors, including the type and severity of the spinal condition, the patient’s overall health, and their response to previous treatments. Based on thorough diagnostic evaluations, treatment options may include minimally invasive spine surgery, spinal fusion, decompression surgery, or advanced procedures like robotic-assisted surgery. Today, spine surgery in India is equipped with state-of-the-art diagnostic tools and advanced surgical techniques. <br/><br/>Contact us to receive the best spine care available.",
+    content: "In emerging development zones."
   },
   {
     id: 5,
-    name: "Farm Houses",
-    image: "FarmHouses.png",
-    content:
-      "India has achieved significant milestones in liver transplant surgery, thanks to our adoption of cutting-edge technologies and the expertise of our skilled surgeons, supported by groundbreaking research. The approach to liver transplant surgery depends on various factors, including the type and severity of liver disease, the patient's overall health, and their response to previous treatments. Comprehensive diagnostic evaluations guide the selection of the most appropriate treatment options, which may include living-donor liver transplants, deceased-donor liver transplants, or advanced minimally invasive techniques. Today, liver transplant surgery in India is equipped with state-of-the-art diagnostic tools and advanced surgical methodologies. <br/><br/>Contact us to access the best liver transplant care available.",
+    // name: "Farm Houses",
+    name: "Angel Investments",
+    image: "angel-investment.png",
+    // content: "India has achieved significant milestones in liver transplant surgery, thanks to our adoption of cutting-edge technologies and the expertise of our skilled surgeons, supported by groundbreaking research. The approach to liver transplant surgery depends on various factors, including the type and severity of liver disease, the patient's overall health, and their response to previous treatments. Comprehensive diagnostic evaluations guide the selection of the most appropriate treatment options, which may include living-donor liver transplants, deceased-donor liver transplants, or advanced minimally invasive techniques. Today, liver transplant surgery in India is equipped with state-of-the-art diagnostic tools and advanced surgical methodologies. <br/><br/>Contact us to access the best liver transplant care available.",
+    content: "Identifying Early-Stage opportunities that can multiply"
   },
   {
     id: 6,
-    name: "Land Parcels",
-    image: "LandParcels.png",
-    content:
-      "India has made significant strides in bone marrow transplant procedures, thanks to the integration of advanced technologies and the expertise of our highly skilled medical professionals, supported by groundbreaking research. The approach to bone marrow transplant depends on various factors, including the type of condition being treated, the patient's overall health, and their compatibility with potential donors. Comprehensive diagnostic assessments guide the selection of the most suitable transplant method, which may include autologous transplantation (using the patient's own cells), allogeneic transplantation (using donor cells), or haploidentical transplantation (using cells from a half-matched donor). Today, bone marrow transplant procedures in India are equipped with state-of-the-art facilities and advanced transplantation techniques. <br/><br/>Contact us to access the best bone marrow transplant care available.",
+    // name: "Land Parcels",
+    name: "Buy-Back Options",
+    image: "buyback.jpg",
+    // content: "India has made significant strides in bone marrow transplant procedures, thanks to the integration of advanced technologies and the expertise of our highly skilled medical professionals, supported by groundbreaking research. The approach to bone marrow transplant depends on various factors, including the type of condition being treated, the patient's overall health, and their compatibility with potential donors. Comprehensive diagnostic assessments guide the selection of the most suitable transplant method, which may include autologous transplantation (using the patient's own cells), allogeneic transplantation (using donor cells), or haploidentical transplantation (using cells from a half-matched donor). Today, bone marrow transplant procedures in India are equipped with state-of-the-art facilities and advanced transplantation techniques. <br/><br/>Contact us to access the best bone marrow transplant care available.",
+    content: "Custom-designed buy-back offers"
   },
-  {
-    id: 7,
-    name: "Holiday Homes",
-    image: "HolidayHomes.png",
-    content:
-      "India has made significant strides in kidney transplant procedures, thanks to advanced technologies and the expertise of our skilled medical professionals, supported by groundbreaking research. The approach to kidney transplant depends on various factors, including the patient's medical history, the type of kidney disease, and their compatibility with potential donors. Thorough diagnostic assessments guide the selection of the most suitable transplant method, which may include living donor kidney transplant, deceased donor kidney transplant, or paired kidney exchange programs. Today, kidney transplant procedures in India are equipped with state-of-the-art facilities and advanced surgical techniques. <br/><br/>Contact us to access the best kidney transplant care available.",
-  },
+  // {
+  //   id: 7,
+  //   name: "Holiday Homes",
+  //   image: "HolidayHomes.png",
+  //   content:
+  //     "India has made significant strides in kidney transplant procedures, thanks to advanced technologies and the expertise of our skilled medical professionals, supported by groundbreaking research. The approach to kidney transplant depends on various factors, including the patient's medical history, the type of kidney disease, and their compatibility with potential donors. Thorough diagnostic assessments guide the selection of the most suitable transplant method, which may include living donor kidney transplant, deceased donor kidney transplant, or paired kidney exchange programs. Today, kidney transplant procedures in India are equipped with state-of-the-art facilities and advanced surgical techniques. <br/><br/>Contact us to access the best kidney transplant care available.",
+  // },
 ];
